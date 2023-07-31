38cc4d5e0c26ac61baed94596de8115f65f7ae58
Vali Zairov <zairov@gmail.com>
2023-07-30 23:50:36 -0700

    Binded UI elements to Controller

--- Tic Tac Game/ViewController.swift
+++ Tic Tac Game/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusTextField: UITextField!
+    @IBOutlet weak var startResetButton: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
+    @IBAction func startResetAction(_ sender: UIButton) {
+        // Start/Reset the game
+    }
+    
+    func setupUI() {
+        //setupBoard()
+        //Reset all squares on UI
+        //Clear status field, change Start Button to Reset
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
 
 }
 
